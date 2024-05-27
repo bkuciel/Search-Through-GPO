ab7793baa70c28b39673654f905df689fb235f82
Bartosz Kuciel <bkuciel@outlook.com>
2024-05-27 17:57:03 +0200

    Rename, Readme, Rewrite_comments

---
+++ README.md
@@ -0,0 +1,12 @@
+# Search for GPO
+"Search for GPO" is a simple script that, based on a keyword, allows you to find GPOs containing the modified setting
+
+## Usage
+
+ - (Optionally) Modify second line with your domain name:
+
+    $Domain  =  "domainA.contoso.local"
+ 
+- Run the script and type word that may be included in modified setting. For example: if you are looking for Password Policy GPOs, type "Password" and script will return all GPOs which are modifying settings related to password.
+
+    What are we looking for?: Password
--- Search-For-GPO.ps1
+++ Search-For-GPO.ps1
@@ -1,8 +1,14 @@
 #Specify domain
 $Domain = "domainA.contoso.local"
 
-$String = Read-Host "Sting: "
- 
+#If domain not specified ask for domain
+if ($Domain -eq "domainA.contoso.local")
+{
+  $Domain = Read-Host "Type domain name: "
+}
+
+$String = Read-Host "What are we looking for?: "
+
 $NearestDC = (Get-ADDomainController -Discover -NextClosestSite).Name
  
 #Get a list of GPOs from the domain
@@ -10,7 +16,8 @@ $GPOs = Get-GPO -All -Domain $Domain -Server $NearestDC | sort DisplayName
 
 $matchingGPOs = $null 
 #Go through each Object and check its XML against $String
-Foreach ($GPO in $GPOs)  {
+Foreach ($GPO in $GPOs)  
+{
   
   Write-Host "Working on $($GPO.DisplayName)"
   
